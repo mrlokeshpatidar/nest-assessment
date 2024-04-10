bcc03813334cc7ebf3009c26707eed9858d03aa6
mrlokeshpatidar <mrlokeshpatidar@gmail.com>
2024-04-10 12:58:32 +0530

    in module db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,14 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      // Your TypeORM configuration
+      type: 'mysql',
+      host: 'host-name',
+      port: 3306,
+      username: 'username',
+      password:'password',
+      database: 'database-name',
+      entities: [FormData],
+      synchronize: false
     }),
     FormModule, 
   ],
