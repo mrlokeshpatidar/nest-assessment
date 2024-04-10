4555f1d01b0676771a0e6edabd9dab1d920e28ce
mrlokeshpatidar <mrlokeshpatidar@gmail.com>
2024-04-10 11:50:34 +0530

    update module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FormModule } from './form/form.module'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FormController } from './form/form.controller';
-import { FormModule } from './form/form.module';
-import { FillController } from './fill/fill.controller';
-import { FillModule } from './fill/fill.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    // TypeORM configuration options
-    // This will automatically load config from ormconfig.json
-  }),FormModule, FillModule],
-  controllers: [AppController, FormController, FillController],
+  imports: [
+    TypeOrmModule.forRoot({
+      // Your TypeORM configuration
+    }),
+    FormModule, 
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/form/form.module.ts
+++ src/form/form.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FormController } from './form.controller';
 import { FormService } from './form.service';
+import { FormData } from '../entities/form.entity';
 
 @Module({
-  providers: [FormService]
+  imports: [TypeOrmModule.forFeature([FormData])], // Import the repository for FormData
+  controllers: [FormController],
+  providers: [FormService],
+
 })
 export class FormModule {}
